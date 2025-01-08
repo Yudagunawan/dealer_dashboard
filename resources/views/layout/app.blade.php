f31e09ce67423c65b9cb945ef738dc3919534f66
yuda <yudagsimanjuntak@gmail.com>
2025-01-08 09:08:52 +0800

    first commit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('title')</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{url("plugins/fontawesome-free/css/all.min.css")}}">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="{{url("plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css")}}">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{url("plugins/icheck-bootstrap/icheck-bootstrap.min.css")}}">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="{{url("plugins/jqvmap/jqvmap.min.css")}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{url("dist/css/adminlte.min.css")}}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{url("plugins/overlayScrollbars/css/OverlayScrollbars.min.css")}}">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{url("plugins/daterangepicker/daterangepicker.css")}}">
+  <!-- summernote -->
+  <link rel="stylesheet" href="{{url("plugins/summernote/summernote-bs4.min.css")}}">
+   <!-- DataTables -->
+   <link rel="stylesheet" href="{{url("plugins/datatables-bs4/css/dataTables.bootstrap4.min.css")}}">
+   <link rel="stylesheet" href="{{url("plugins/datatables-responsive/css/responsive.bootstrap4.min.css")}}">
+   <link rel="stylesheet" href="{{url("plugins/datatables-buttons/css/buttons.bootstrap4.min.css")}}">
+  
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{url("dist/img/AdminLTELogo.png")}}" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+     
+      <!-- Notifications Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fas fa-cog"></i>
+          <span class="badge badge-warning navbar-badge"></span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          
+          <div class="dropdown-divider"></div>
+          <a href="{{route('logout')}}" class="dropdown-item">
+            <i class="fas fa-sign-out-alt mr-2"></i> logout
+            <span class="float-right text-muted text-sm"></span>
+          </a>
+          <div class="dropdown-divider"></div>
+          {{-- <a href="#" class="dropdown-item">
+            <i class="fas fa-users mr-2"></i> 8 friend requests
+            <span class="float-right text-muted text-sm">12 hours</span>
+          </a> --}}
+          <div class="dropdown-divider"></div>
+          
+          <div class="dropdown-divider"></div>
+         
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+     
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="{{url("/dashboard")}}" class="brand-link">
+      <img src="{{url("dist/img/AdminLTELogo.png")}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Admin</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="{{url("dist/img/default-profile.jpg")}}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">{{Auth::user()->username}}</a>
+        </div>
+      </div>
+
+      
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="{{url("/dashboard")}}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                
+              </p>  
+            </a>
+            
+          </li>
+          <li class="nav-item">
+            <a href="{{url("/upload")}}" class="nav-link">
+              <i class="nav-icon fas fa-file"></i>
+              <p> 
+                Upload
+                {{-- <span class="right badge badge-danger">New</span> --}}
+              </p>
+            </a>
+          </li>
+        </li>
+        <li class="nav-item">
+          <a href="{{route("upload.list")}}" class="nav-link">
+            <i class="nav-icon fas fa-list-alt"></i>
+            <p>
+              List Upload
+              {{-- <span class="right badge badge-danger">New</span> --}}
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{route("users.list")}}" class="nav-link">
+            <i class="nav-icon fas fa-users"></i>
+            <p>
+              Users List
+              {{-- <span class="right badge badge-danger">New</span> --}}
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{route("departments")}}" class="nav-link">
+            <i class="nav-icon fas fa-university"></i>
+            <p>
+              Departments
+              {{-- <span class="right badge badge-danger">New</span> --}}
+            </p>
+          </a>
+        </li>
+          {{-- <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Layout Options
+                <i class="fas fa-angle-left right"></i>
+                <span class="badge badge-info right">6</span>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/layout/top-nav.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Top Navigation</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Top Navigation + Sidebar</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/boxed.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Boxed</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/fixed-sidebar.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Fixed Sidebar</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/fixed-sidebar-custom.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Fixed Sidebar <small>+ Custom Area</small></p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/fixed-topnav.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Fixed Navbar</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/fixed-footer.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Fixed Footer</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/collapsed-sidebar.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Collapsed Sidebar</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          --}}
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  @yield('content')
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2024.</strong>
+    All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{url("plugins/jquery/jquery.min.js")}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{url("plugins/jquery-ui/jquery-ui.min.js")}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+
+<!-- Bootstrap 4 -->
+<script src="{{url("plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+<!-- ChartJS -->
+<script src="{{url("plugins/chart.js/Chart.min.js")}}"></script>
+<!-- Sparkline -->
+<script src="{{url("plugins/sparklines/sparkline.js")}}"></script>
+<!-- JQVMap -->
+<script src="{{url("plugins/jqvmap/jquery.vmap.min.js")}}"></script>
+<script src="{{url("plugins/jqvmap/maps/jquery.vmap.usa.js")}}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{url("plugins/jquery-knob/jquery.knob.min.js")}}"></script>
+<!-- daterangepicker -->
+<script src="{{url("plugins/moment/moment.min.js")}}"></script>
+<script src="{{url("plugins/daterangepicker/daterangepicker.js")}}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{url("plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js")}}"></script>
+<!-- Summernote -->
+<script src="{{url("plugins/summernote/summernote-bs4.min.js")}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{url("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js")}}"></script>
+<!-- AdminLTE App -->
+<script src="{{url("dist/js/adminlte.js")}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{url("dist/js/demo.js")}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{url("dist/js/pages/dashboard.js")}}"></script>
+<!-- DataTables  & Plugins -->
+<script src="{{url("plugins/datatables/jquery.dataTables.min.js")}}"></script>
+<script src="{{url("plugins/datatables-bs4/js/dataTables.bootstrap4.min.js")}}"></script>
+<script src="{{url("plugins/datatables-responsive/js/dataTables.responsive.min.js")}}"></script>
+<script src="{{url("plugins/datatables-responsive/js/responsive.bootstrap4.min.js")}}"></script>
+<script src="{{url("plugins/datatables-buttons/js/dataTables.buttons.min.js")}}"></script>
+<script src="{{url("plugins/datatables-buttons/js/buttons.bootstrap4.min.js")}}"></script>
+<script src="{{url("plugins/jszip/jszip.min.js")}}"></script>
+<script src="{{url("plugins/pdfmake/pdfmake.min.js")}}"></script>
+<script src="{{url("plugins/pdfmake/vfs_fonts.js")}}"></script>
+<script src="{{url("plugins/datatables-buttons/js/buttons.html5.min.js")}}"></script>
+<script src="{{url("plugins/datatables-buttons/js/buttons.print.min.js")}}"></script>
+<script src="{{url("plugins/datatables-buttons/js/buttons.colVis.min.js")}}"></script>
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+<script>
+  $(document).on('click', '.toggle-status', function () {
+const button = $(this);
+const projectId = button.data('id');
+
+$.ajax({
+    url: `/project/${projectId}/toggle-status`,
+    type: 'PATCH',
+    headers: { 'X-CSRF-TOKEN': '{{ csrf_token() }}' },
+    success: function (response) {
+        if (response.status_link) {
+            // Update button UI
+            const newStatus = response.status_link;
+            button.data('status', newStatus);
+            button.text(newStatus.charAt(0).toUpperCase() + newStatus.slice(1));
+            button.css('background-color', newStatus === 'aktif' ? 'green' : 'red');
+        } else {
+            alert('Gagal mendapatkan status baru');
+        }
+    },
+    error: function (xhr) {
+        console.error(xhr.responseText);
+        alert('Gagal mengubah status');
+    }
+});
+});
+
+
+</script>
+  
+</body>
+</html>
+
